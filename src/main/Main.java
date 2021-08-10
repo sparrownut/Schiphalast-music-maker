19c891884e126cb3cee1f97e1d06465bf1193c6a
stupidfish <2928109164@qq.com>
2021-08-10 16:44:01 +0800

    Initial commit

---
+++ src/main/Main.java
@@ -0,0 +1,23 @@
+package main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("mnain.fxml"));
+        primaryStage.setTitle("Schiphalast铺面编辑器");
+        primaryStage.setScene(new Scene(root, 1920, 1080));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/com/stupidfish/cache/ChartCache.kt
@@ -0,0 +1,4 @@
+package com.stupidfish.cache
+
+class ChartCache {
+}
