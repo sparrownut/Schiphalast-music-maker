20e8991bfabb759131e27bcb878081eea2e34471
stupidfish <2928109164@qq.com>
2021-08-10 17:09:22 +0800

    数据结构2

--- src/main/java/com/stupidfish/cache/NoteCache.kt
+++ src/main/java/com/stupidfish/cache/NoteCache.kt
@@ -1,17 +1,31 @@
 package com.stupidfish.cache
 
+import com.alibaba.fastjson.JSONObject
+import com.stupidfish.cache.NoteType.*
+
 class NoteCache {
     /**
      * Note的存储类
      */
-    val type = NoteType.N //Default
+    val type = N //Default
     val time = 0
     val BPM = 1
     val yPos = 0
     val endT = 0
     val rs = 0
     val rd = 0
-    fun getNoteJson(){
-
-    }
+//    fun getNoteData() {
+//        val map = mapOf<NoteCache>()
+//        when(type){
+//            N -> {
+//            }
+//            NT -> TODO()
+//            F -> TODO()
+//            FR -> {
+//
+//            }
+//            H -> TODO()
+//        }
+//
+//    }
 }
